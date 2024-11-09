5f38d45e01b7763d18632568826986aad564c6d7
ornobaadi <aadi4789@gmail.com>
2024-11-09 19:54:14 +0600

    Register

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">Welcome to Email Password Auth Home</h2>
+        </div>
+    );
+};
+
+export default Home;
