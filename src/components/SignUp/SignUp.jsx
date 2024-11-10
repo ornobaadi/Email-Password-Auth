bbbbefeee7659d2abe539605435200e658daf0db
ornobaadi <aadi4789@gmail.com>
2024-11-10 13:00:22 +0600

    Accept terms and validation

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,13 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 
 const SignUp = () => {
-
+    const [success, setSuccess] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
 
     const handleSignUp = e => {
@@ -12,19 +15,40 @@ const SignUp = () => {
 
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        const terms = e.target.terms.checked;
+        console.log(email, password, terms);
 
         // Clear the message
         setErrorMessage('');
+        setSuccess(false);
+
+        if(!terms){
+            setErrorMessage('Please accept terms and conditions to proceed');
+            return;
+        }
+
+        if (password.length < 6) {
+            setErrorMessage('Password should be 6 characters or longer');
+            return;
+        }
+
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+        if (!passwordRegex.test(password)) {
+            setErrorMessage('Required: At least one uppercase, one lowercase, one number, one special character');
+            return;
+        }
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result =>{
-            console.log(result.user);
-        })
-        .catch(error => {
-            console.log('ERROR', error.message);
-            setErrorMessage(error.message)
-        })
+            .then(result => {
+                console.log(result.user);
+                setSuccess(true)
+            })
+            .catch(error => {
+                console.log('ERROR', error.message);
+                setErrorMessage(error.message);
+                setSuccess(false);
+            })
     }
 
 
@@ -32,19 +56,39 @@ const SignUp = () => {
         <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
             <h1 className="text-2xl ml-4  font-bold">Sign Up now!</h1>
             <form onSubmit={handleSignUp} className="card-body">
+            {/* Email  */}
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
                     <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                 </div>
-                <div className="form-control">
+            {/* Password  */}
+                <div className="form-control relative">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        name="password"
+                        placeholder="password"
+                        className="input input-bordered" required />
+            {/* Show Password  */}
+                    <button type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="btn btn-xs absolute right-2 top-12">
+                        {
+                            showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                        }
+                    </button>
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label className="label justify-start cursor-pointer gap-3">
+                        <input type="checkbox" name="terms" className="checkbox" />
+                        <span className="label-text">Accept our terms and conditions.</span>
                     </label>
                 </div>
                 <div className="form-control mt-6">
@@ -53,6 +97,9 @@ const SignUp = () => {
             </form>
             {
                 errorMessage && <p className="text-red-600">{errorMessage}</p>
+            }
+            {
+                success && <p className="text-green-600">Account Successfully Created</p>
             }
         </div>
     );
